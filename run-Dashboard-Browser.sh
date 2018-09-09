9a88cff8ae3230aad6359ffb5337bf0b67594a2c
DrSnowbird <openkbs.org@gmail.com>
2018-09-09 15:14:08 -0400

    update scripts with minor fixes

--- run-Dashboard-Browser.sh
+++ run-Dashboard-Browser.sh
@@ -2,26 +2,25 @@
 
 # reference: https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/#deploying-the-dashboard-ui
 
-
 # How to access Dashboard of Kubernete?
 
 echo "## ... 1.) see kube-system pod .........."
-sudo kubectl get pods --namespace=kube-system
+kubectl get pods --namespace=kube-system
 
 echo "## ... 2.) see kube-system svc .........."
-sudo kubectl get svc --namespace=kube-system
+kubectl get svc --namespace=kube-system|
 
 echo "## ... 3.) find Dashboard UI web port  .........."
-DASHBOARD_PORT=`sudo kubectl get svc --namespace=kube-system|grep 'kubernetes-dashboard' | cut -d':' -f2 | cut -d'/' -f1 `
-echo "DASHBOARD_PORT=${DASHBOARD_PORT}"
+
+DASHBOARD_PORT=`kubectl get svc --namespace=kube-system|grep 'kubernetes-dashboard' | cut -d':' -f2 | cut -d'/' -f1 `
+echo "DASHBOARD_PORT=${DASHBOARD_PORT:-30000}"
 
 echo "## ... 4.) launch your Firefox or Chrome  .........."
 # /usr/bin/google-chrome http://127.0.0.1:30000/
 
 if `which google-chrome`; then
-    /usr/bin/google-chrome http://127.0.0.1:${DASHBOARD_PORT}/
+    /usr/bin/google-chrome http://192.168.99.100:${DASHBOARD_PORT}/ &
 else
-    firefox http://127.0.0.1:${DASHBOARD_PORT}/
+    firefox http://192.168.99.100:${DASHBOARD_PORT}/ &
 fi
-
 
